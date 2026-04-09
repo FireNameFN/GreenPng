7e3c9c19c53259365feb2feefd62948c0c064e6b
FN <firenamefn@gmail.com>
2026-04-09 19:46:20 +0700

    Update tests

--- GreenPng.Tests/DecodeTests.cs
+++ GreenPng.Tests/DecodeTests.cs
@@ -6,24 +6,6 @@ using GreenPng.Testing;
 namespace GreenPng.Tests;
 
 public sealed class DecodeTests {
-    [Test, Skip("Magick.NET is incorrect.")]
-    [MethodDataSource(typeof(TestImageDataSource), nameof(TestImageDataSource.GetTestImages))]
-    public async Task EqualToImageMagick(TestImage testImage) {
-        byte[] image = Decoders.DecodeGreenPng(testImage.Png);
-
-        byte[] imageRef = Decoders.DecodeImageMagick(testImage.Png);
-
-        int compare = 0;
-
-        for(int i = 0; i < image.Length; i++)
-            if(image[i] == imageRef[i])
-                compare++;
-            else
-                break;
-
-        await Assert.That(image.SequenceEqual(imageRef)).IsTrue();
-    }
-
     [Test]
     [MethodDataSource(typeof(TestImageDataSource), nameof(TestImageDataSource.GetTestImages))]
     public async Task EqualToStbImageSharp(TestImage testImage) {
